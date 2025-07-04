0332530d965706da8fd7228c5834da1c69fcab6e
Ricardominom <110798786+Ricardominom@users.noreply.github.com>
2025-07-04 14:51:36 -0600

    Update licenses text to 'en el mes'

--- src/components/MiDashboard.tsx
+++ src/components/MiDashboard.tsx
@@ -129,7 +129,7 @@ const MiDashboard: React.FC = () => {
             <div className="grid grid-cols-3 gap-4 text-center">
               <div>
                 <div className="text-3xl font-bold text-emerald-600">189</div>
-                <div className="text-xs text-gray-500">Licencias otorgadas por mes</div>
+                <div className="text-xs text-gray-500">Licencias otorgadas en el mes</div>
               </div>
               <div>
                 <div className="text-3xl font-bold text-emerald-600">400</div>
